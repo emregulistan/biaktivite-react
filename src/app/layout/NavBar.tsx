4a426ebdb0c92e995c9080096a8b5bd62d107b97
emregulistan <emregulistan1@gmail.com>
2022-08-03 19:38:27 +0300

    mobx

--- src/app/layout/NavBar.tsx
+++ src/app/layout/NavBar.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
-interface Props {
-    openForm: () => void;
-}
+export default function NavBar() {
+    const {activityStore} = useStore();
 
-export default function NavBar({openForm}: Props) {
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '20px'}} />
-                    BiAktivite
+                <Menu.Item as={NavLink} to='/' exact header>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
+                    Reactivities
                 </Menu.Item>
-                <Menu.Item name='Aktiviteler' />
+                <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item>
-                    <Button onClick={openForm} positive content='Aktivite oluştur' />
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,49 +1,34 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props {
-    activities: Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id: string) => void;
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityDashboard() {
 
-export default function ActivityDashboard({activities, selectedActivity, deleteActivity,
-        selectActivity, cancelSelectActivity, editMode, openForm, 
-        closeForm, createOrEdit, submitting}: Props) {
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
+
+    useEffect(() => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} 
-                    selectActivity={selectActivity} 
-                    deleteActivity={deleteActivity}
-                    submitting={submitting}
-                />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
-                <ActivityDetails 
-                    activity={selectedActivity} 
-                    cancelSelectActivity={cancelSelectActivity}
-                    openForm={openForm} 
-                />}
+                <ActivityDetails />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm}
-                 activity={selectedActivity} 
-                 createOrEdit={createOrEdit} 
-                 submitting={submitting}
-                 />}
+                <ActivityForm />}
             </Grid.Column>
         </Grid>
     )
-}
+})
